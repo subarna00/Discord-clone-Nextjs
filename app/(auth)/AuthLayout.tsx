4eb4bdc89e39f30cdcb39034ea7bd5b3d34fe0cf
subarna00 <subarnauprety@gmail.com>
2023-12-27 17:39:32 +0545

    clerk implementation

--- app/(auth)/AuthLayout.tsx
+++
@@ -1,5 +0,0 @@
-const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
-};
-
-export default AuthLayout;
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='flex items-center justify-center h-full bg-indigo-900'>
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
